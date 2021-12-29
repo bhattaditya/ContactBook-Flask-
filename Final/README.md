70a85b0bb36523cb6ba3ecfd14c967d5e2280ccf
Aditya Bhatt <66739975+bhattaditya@users.noreply.github.com>
2021-12-29 16:16:13 +0530

    Delete Final directory

--- Final/README.md
+++
@@ -1,1 +0,0 @@
->Contacts can be added, updated,deleted and can be searched by email address.
--- Final/run.py
+++
@@ -1,7 +0,0 @@
-
-from contactbook import app, db
-
-if __name__ == "__main__":
-    db.create_all()
-    app.run(debug=True)
-
